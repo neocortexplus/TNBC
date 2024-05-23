a92d74f74f5427d411e681f7527623d3ef011a0b
rebwar karimi <r.karimi.ml@gmail.com>
2024-05-23 06:24:19 +0000

    init code

---
+++ microarray_analysis.R
@@ -0,0 +1,258 @@
+# Version info: R 4.2.2, Biobase 2.58.0, GEOquery 2.66.0, limma 3.54.0
+################################################################
+#   Differential expression analysis with limma
+library(GEOquery)
+library(limma)
+library(umap)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(Biobase)
+
+# load series and platform data from GEO
+# 
+# gset <- tryCatch({
+#   getGEO("GSE76250", GSEMatrix = TRUE, AnnotGPL = TRUE)
+# }, error = function(e) e)
+
+
+gset <- getGEO("GSE76250", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL17586", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# make proper column names to match toptable 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+# group membership for all samples
+gsms <- paste0("00000000000000000000000000000000000000000000000000",
+               "00000000000000000000000000000000000000000000000000",
+               "00000000000000000000000000000000000000000000000000",
+               "000000000000000111111111111111111111111111111111")
+
+
+
+sml <- strsplit(gsms, split="")[[1]]
+
+# log2 transformation
+ex <- exprs(gset)
+qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
+LogC <- (qx[5] > 100) ||
+  (qx[6]-qx[1] > 50 && qx[2] > 0)
+if (LogC) { ex[which(ex <= 0)] <- NaN
+exprs(gset) <- log2(ex) }
+
+
+
+ex_df <- as.data.frame(ex)
+feature_data <- fData(gset)
+pheno_data <- pData(phenoData(gset))
+
+# Assuming 'feature_data' is your dataframe and 'gene_assignment' is the column with the data
+
+
+feature_data$Gene.Symbol <- sapply(strsplit(as.character(feature_data$gene_assignment), " // "), function(x) x[2])
+feature_data$Gene.ID <- sapply(strsplit(as.character(feature_data$gene_assignment), " // "), function(x) x[5])
+
+feature_data$Gene.ID1 <- sapply(strsplit(as.character(feature_data$gene_assignment), " // "), function(x) x[1])
+feature_data$Gene.Symbol1 <- sapply(strsplit(as.character(feature_data$gene_assignment), " // "), function(x) x[2])
+
+feature_data$Gene.ID <- sapply(strsplit(as.character(feature_data$Gene.ID), " /// "), function(x) x[1])
+
+
+count_missing <- function(x) {
+  sum(is.na(x) | x %in% c("--", "nan", "NA", "", " ", "---"))
+}
+
+# Apply this function to each of the specified columns
+
+missing_counts <- sapply(feature_data[c("Gene.ID", "Gene.Symbol", "Gene.ID1", "Gene.Symbol1")], count_missing)
+
+print(missing_counts)
+
+
+feature_data <- feature_data %>%
+  filter(!is.na(Gene.Symbol) & !Gene.Symbol %in% c("--", "nan", "NA", "", " ", "---"))
+
+
+# feature_data <- feature_data[, c("ID", "Gene.ID", "Gene.symbol", "Platform_SPOTID")]
+feature_data <- feature_data[, c("ID", "Gene.Symbol")]
+
+combined_data <- merge(ex_df, feature_data, by.x = "row.names", by.y = "ID")
+# Rename the 'Row.names' column to 'ID' in the resulting data frame for clarity
+names(combined_data)[1] <- "ID"
+
+
+
+
+aggregated_data <- combined_data %>%
+  group_by(Gene.Symbol) %>%
+  summarise(across(starts_with("GSM"), mean, na.rm = TRUE))
+
+
+aggregated_data <- as.data.frame(aggregated_data)
+
+
+feature_data <- feature_data[!duplicated(feature_data$Gene.Symbol), ]
+
+aggregated_data_with_ids <- aggregated_data %>%
+  left_join(feature_data, by = "Gene.Symbol")
+
+# Check the first few rows of the new dataframe to confirm the merge
+head(aggregated_data_with_ids)
+
+
+
+fs_data <- aggregated_data_with_ids[, c("ID", "Gene.Symbol")]
+row.names(aggregated_data_with_ids) <- aggregated_data_with_ids$Gene.Symbol
+
+aggregated_data_with_ids <- aggregated_data_with_ids %>%
+  select(-ID, -Gene.Symbol)
+
+
+
+expression_matrix <- as.matrix(aggregated_data_with_ids)
+
+rownames(fs_data) <- fs_data$Gene.Symbol
+
+
+new_gset <- ExpressionSet(
+  assayData = expression_matrix,       # Replace 'expression_matrix' with your actual matrix of expression data
+  phenoData = phenoData(gset),         # Use the 'phenoData' directly if it is already an AnnotatedDataFrame
+  featureData = AnnotatedDataFrame(fs_data)  # Replace 'fs_data' with your feature data
+)
+
+# Inspecting the new ExpressionSet
+
+gset <- new_gset
+
+
+
+
+
+# assign samples to groups and set up design matrix
+gs <- factor(sml)
+groups <- make.names(c("TNBC","normal"))
+levels(gs) <- groups
+gset$group <- gs
+
+
+
+
+
+
+design <- model.matrix(~group + 0, gset)
+colnames(design) <- levels(gs)
+
+
+
+
+
+gset <- gset[complete.cases(exprs(gset)), ] # skip missing values
+
+fit <- lmFit(gset, design)  # fit linear model
+
+# set up contrasts of interest and recalculate model coefficients
+cts <- paste(groups[1], groups[2], sep="-")
+cont.matrix <- makeContrasts(contrasts=cts, levels=design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+
+# compute statistics and table of top significant genes
+fit2 <- eBayes(fit2, 0.01)
+tT <- topTable(fit2, adjust="fdr", sort.by="B", number=250)
+
+tT <- subset(tT, select=c("ID" ,"Gene.Symbol","adj.P.Val","P.Value","t","B","logFC"))
+write.table(tT, file=stdout(), row.names=F, sep="\t")
+
+
+
+
+
+# Visualize and quality control test results.
+# Build histogram of P-values for all genes. Normal test
+# assumption is that most genes are not differentially expressed.
+tT2 <- topTable(fit2, adjust="fdr", sort.by="B", number=Inf)
+hist(tT2$adj.P.Val, col = "grey", border = "white", xlab = "P-adj",
+     ylab = "Number of genes", main = "P-adj value distribution")
+
+# summarize test results as "up", "down" or "not expressed"
+dT <- decideTests(fit2, adjust.method="fdr", p.value=0.05, lfc=0)
+
+# Venn diagram of results
+vennDiagram(dT, circle.col=palette())
+
+# create Q-Q plot for t-statistic
+t.good <- which(!is.na(fit2$F)) # filter out bad probes
+qqt(fit2$t[t.good], fit2$df.total[t.good], main="Moderated t statistic")
+
+# volcano plot (log P-value vs log fold change)
+colnames(fit2) # list contrast names
+ct <- 1        # choose contrast of interest
+# Please note that the code provided to generate graphs serves as a guidance to
+# the users. It does not replicate the exact GEO2R web display due to multitude
+# of graphical options.
+# 
+# The following will produce basic volcano plot using limma function:
+volcanoplot(fit2, coef=ct, main=colnames(fit2)[ct], pch=20,
+            highlight=length(which(dT[,ct]!=0)), names=rep('+', nrow(fit2)))
+
+# MD plot (log fold change vs mean log expression)
+# highlight statistically significant (p-adj < 0.05) probes
+plotMD(fit2, column=ct, status=dT[,ct], legend=F, pch=20, cex=1)
+abline(h=0)
+
+################################################################
+# General expression data analysis
+ex <- exprs(gset)
+
+# box-and-whisker plot
+dev.new(width=3+ncol(gset)/6, height=5)
+ord <- order(gs)  # order samples by group
+palette(c("#1B9E77", "#7570B3", "#E7298A", "#E6AB02", "#D95F02",
+          "#66A61E", "#A6761D", "#B32424", "#B324B3", "#666666"))
+par(mar=c(7,4,2,1))
+title <- paste ("GSE76250", "/", annotation(gset), sep ="")
+boxplot(ex[,ord], boxwex=0.6, notch=T, main=title, outline=FALSE, las=2, col=gs[ord])
+legend("topleft", groups, fill=palette(), bty="n")
+dev.off()
+
+# expression value distribution
+par(mar=c(4,4,2,1))
+title <- paste ("GSE76250", "/", annotation(gset), " value distribution", sep ="")
+plotDensities(ex, group=gs, main=title, legend ="topright")
+
+# UMAP plot (dimensionality reduction)
+ex <- na.omit(ex) # eliminate rows with NAs
+ex <- ex[!duplicated(ex), ]  # remove duplicates
+ump <- umap(t(ex), n_neighbors = 15, random_state = 123)
+par(mar=c(3,3,2,6), xpd=TRUE)
+plot(ump$layout, main="UMAP plot, nbrs=15", xlab="", ylab="", col=gs, pch=20, cex=1.5)
+legend("topright", inset=c(-0.15,0), legend=levels(gs), pch=20,
+       col=1:nlevels(gs), title="Group", pt.cex=1.5)
+library("maptools")  # point labels without overlaps
+pointLabel(ump$layout, labels = rownames(ump$layout), method="SANN", cex=0.6)
+
+# mean-variance trend, helps to see if precision weights are needed
+plotSA(fit2, main="Mean variance trend, GSE76250")
+
+
+
+
+gene_data <- as.data.frame(t(exprs(gset)))
+pheno_data <- pData(phenoData(gset))
+
+complete <- merge(gene_data, pheno_data, by.x = "row.names", by.y = "row.names")
+
+data_to_plot <- complete %>%
+  select(TOP2A, group)  
+
+# Create the plot
+ggplot(data_to_plot, aes(x = group, y = TOP2A, color = group)) +
+  geom_boxplot() +
+  labs(title = "Expression of TOP2A in TNBC and Normal Samples",
+       x = "Sample Group",
+       y = "Expression Level") +
+  theme_minimal()  # Adds a minimalistic theme to the plot
+
+# Print the plot
+print(ggplot_object)
+
